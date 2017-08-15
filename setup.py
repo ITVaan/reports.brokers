1f067e2bcbf2bce56bfd3ea55ad6d4d60f27c53f
Shevchenko Vlad <you@example.com>
2017-08-15 11:45:03 +0300

    buildout construction

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+requires = [
+    'redis',
+    'setuptools',
+]
+
+test_requires = requires + [
+    'webtest',
+    'python-coveralls',
+    'mock==1.0.1',
+    'requests_mock==1.3.0',
+    'bottle'
+]
+
+databridge_requires = requires + [
+    'PyYAML',
+    'gevent',
+    'redis',
+    'LazyDB',
+    'ExtendedJournalHandler',
+    'requests',
+    'openprocurement_client>=1.0b2'
+]
+
+setup(name='reports.brokers.bridge',
+      version=version,
+      description="",
+      long_description=open("README.rst").read(),
+      classifiers=[
+        "Framework :: Pylons",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+        ],
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      url='https://github.com/ITVaan/reports.brokers.bridge',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=test_requires,
+      extras_require={'databridge': databridge_requires,
+                      'test': test_requires},
+      )

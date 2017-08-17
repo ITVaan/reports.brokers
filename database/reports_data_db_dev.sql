0736b844d9c69c47e93f4240ccac503680dc90f9
SD <denisov.univ@gmail.com>
2017-08-17 08:18:48 +0300

    modified sp, test caller script. Escaped unicode problem.

--- database/reports_data_db_dev.sql
+++ database/reports_data_db_dev.sql
@@ -19,14 +19,7 @@ CREATE TABLE `brokers` (
   `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `broker_code` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
-/*Data for the table `brokers` */
-
-insert  into `brokers`(`id`,`code`,`created_on`) values (1,'prom.ua','2017-08-15 11:38:34');
-insert  into `brokers`(`id`,`code`,`created_on`) values (2,'netcast.com.ua','2017-08-15 11:38:55');
-insert  into `brokers`(`id`,`code`,`created_on`) values (3,'netcast.com.ua.test','2017-08-15 18:01:01');
-insert  into `brokers`(`id`,`code`,`created_on`) values (4,'it.ua','2017-08-15 18:38:49');
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `procurement_method_types` */
 
@@ -37,15 +30,6 @@ CREATE TABLE `procurement_method_types` (
   UNIQUE KEY `code` (`code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
-/*Data for the table `procurement_method_types` */
-
-insert  into `procurement_method_types`(`id`,`code`) values (2,'aboveThresholdEU');
-insert  into `procurement_method_types`(`id`,`code`) values (1,'aboveThresholdUA');
-insert  into `procurement_method_types`(`id`,`code`) values (3,'aboveThresholdUA.defense');
-insert  into `procurement_method_types`(`id`,`code`) values (6,'belowThreshold');
-insert  into `procurement_method_types`(`id`,`code`) values (5,'competitiveDialogueEU.stage2');
-insert  into `procurement_method_types`(`id`,`code`) values (4,'competitiveDialogueUA.stage2');
-
 /*Table structure for table `statuses` */
 
 CREATE TABLE `statuses` (
@@ -53,18 +37,7 @@ CREATE TABLE `statuses` (
   `code` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `code` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-
-/*Data for the table `statuses` */
-
-insert  into `statuses`(`id`,`code`) values (3,'active.auction');
-insert  into `statuses`(`id`,`code`) values (5,'active.awarded');
-insert  into `statuses`(`id`,`code`) values (1,'active.enquiries');
-insert  into `statuses`(`id`,`code`) values (4,'active.qualification');
-insert  into `statuses`(`id`,`code`) values (2,'active.tendering');
-insert  into `statuses`(`id`,`code`) values (8,'cancelled');
-insert  into `statuses`(`id`,`code`) values (7,'complete');
-insert  into `statuses`(`id`,`code`) values (6,'unsuccessful');
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `tenders` */
 
@@ -84,12 +57,7 @@ CREATE TABLE `tenders` (
   KEY `FK_tender_status` (`status_id`),
   CONSTRAINT `FK_tender_broker` FOREIGN KEY (`broker_id`) REFERENCES `brokers` (`id`),
   CONSTRAINT `FK_tender_status` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
-/*Data for the table `tenders` */
-
-insert  into `tenders`(`id`,`original_id`,`status_id`,`broker_id`,`date_modified`,`created_on`,`updated_on`,`enquiry_start_date`,`enquiry_end_date`) values (5,'68072d2ab3e54b1e941cab880d9f839e',5,2,'2017-06-19 18:07:10.351145','2017-08-15 18:00:46.913','2017-08-15 18:37:20.014','2017-05-18 14:39:20.351569','2017-05-20 14:44:19');
-insert  into `tenders`(`id`,`original_id`,`status_id`,`broker_id`,`date_modified`,`created_on`,`updated_on`,`enquiry_start_date`,`enquiry_end_date`) values (6,'87d531aefbcf413c838ba0d7dd573b6a',6,4,'2017-08-14 21:04:25.023836','2017-08-15 18:51:03.207','2017-08-15 18:51:17.918','2017-07-12 06:24:02.000000','2017-07-17 12:00:00');
+) ENGINE=InnoDB AUTO_INCREMENT=302 DEFAULT CHARSET=utf8;
 
 /* Procedure structure for procedure `sp_check_tender` */
 
@@ -111,8 +79,12 @@ DELIMITER $$
 
 /*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_update_tender`(
 	IN `tender_json` LONGTEXT,
+	IN `modified_date_string` VARCHAR(50),
 	OUT `error_code` INT,
 	OUT `error_description` VARCHAR(100)
+
+
+
 
 
 
@@ -156,7 +128,7 @@ BEGIN
 	end if;
 		
 	if error_code = 0 then	
-		set @dateString = JSON_VALUE(tender_json,'$.data.dateModified'); # with microseconds
+		set @dateString = modified_date_string; # with microseconds
 		IF SUBSTRING(@dateString, 27, 1) = '+' or SUBSTRING(@dateString, 27, 1) = '-' THEN 
 			set @timeZone = SUBSTRING(@dateString, 27, 6);
 		end if;	
@@ -195,6 +167,7 @@ BEGIN
 			
 			if @brokerCode is null or @brokerCode = '' then
 				set error_code = -1;
+				set error_description = CONCAT('Bad broker code: ', IFNULL(@brokerCode, 'NULL!'));
 			else 
 				insert into brokers (code) values (@brokerCode); # not in tran, possibly can be inserted already by another query
 			
@@ -214,8 +187,8 @@ BEGIN
 		set @id := (select id FROM tenders WHERE original_id = @tenderHashId);		
 		if @id <> 0 then
 			update tenders set 
-				status_id = (select id ),
-				broker_id = (select id from brokers where code = JSON_VALUE(tender_json,'$.data.owner')),
+				status_id = @statusId,
+				broker_id = @brokerId,
 				date_modified = @tenderDateModified,
 				enquiry_start_date = @enquiryPeriodStartDate,
 				enquiry_end_date =  @enquiryPeriodEndDate,
@@ -224,8 +197,7 @@ BEGIN
 		else
 			insert into tenders (original_id, status_id, broker_id, date_modified, enquiry_start_date, enquiry_end_date)
 			values (
-				@tenderHashId, (select id from statuses where code = JSON_VALUE(tender_json,'$.data.status')),
-				(select id from brokers where code = JSON_VALUE(tender_json,'$.data.owner')),
+				@tenderHashId, @statusId, @brokerId,
 				@tenderDateModified, @enquiryPeriodStartDate, @enquiryPeriodEndDate
 			);
 		end if;
---
+++ database/reports_data_dics_dev.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v12.14 (64 bit)
+MySQL - 10.2.7-MariaDB : Database - reports_data
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Data for the table `procurement_method_types` */
+
+insert  into `procurement_method_types`(`id`,`code`) values (2,'aboveThresholdEU');
+insert  into `procurement_method_types`(`id`,`code`) values (1,'aboveThresholdUA');
+insert  into `procurement_method_types`(`id`,`code`) values (3,'aboveThresholdUA.defense');
+insert  into `procurement_method_types`(`id`,`code`) values (6,'belowThreshold');
+insert  into `procurement_method_types`(`id`,`code`) values (5,'competitiveDialogueEU.stage2');
+insert  into `procurement_method_types`(`id`,`code`) values (4,'competitiveDialogueUA.stage2');
+
+/*Data for the table `statuses` */
+
+insert  into `statuses`(`id`,`code`) values (3,'active.auction');
+insert  into `statuses`(`id`,`code`) values (5,'active.awarded');
+insert  into `statuses`(`id`,`code`) values (1,'active.enquiries');
+insert  into `statuses`(`id`,`code`) values (9,'active.pre-qualification');
+insert  into `statuses`(`id`,`code`) values (4,'active.qualification');
+insert  into `statuses`(`id`,`code`) values (2,'active.tendering');
+insert  into `statuses`(`id`,`code`) values (8,'cancelled');
+insert  into `statuses`(`id`,`code`) values (7,'complete');
+insert  into `statuses`(`id`,`code`) values (6,'unsuccessful');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

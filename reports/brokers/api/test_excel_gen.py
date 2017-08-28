3e76159377f27a4584134daf54ccfe4018355ff3
Shevchenko Vlad <you@example.com>
2017-08-28 15:34:31 +0300

    Created api files

--- reports/brokers/api/test_excel_gen.py
+++ reports/brokers/api/test_excel_gen.py
@@ -44,8 +44,8 @@ conn = mariadb.connect(host='127.0.0.1', user='root', password='root', database=
 cursor = conn.cursor(buffered=True)
 
 res = cursor.execute(sql)
-for (broker_name, suppliers_count) in cursor:
-  data.append([broker_name, suppliers_count])
+for broker_name, suppliers_count in cursor:
+    data.append([broker_name, suppliers_count])
 
 cursor.close()
 conn.close()
@@ -55,19 +55,19 @@ result_dir = 'reports'
 template_file_name = 'one.xlsx'
 
 t = os.path.splitext(template_file_name)
-result_file = os.path.join(result_dir, t[0] +'-'+ datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + t[1])
+result_file = os.path.join(result_dir, t[0] + '-' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + t[1])
 
 copyfile(os.path.join(templates_dir, template_file_name), result_file)
 
-wb = load_workbook(filename = result_file)
+wb = load_workbook(filename=result_file)
 ws = wb.active
 
 row = 2
 
 for (broker_name, suppliers_count) in data:
-  ws.cell(row=row, column=1, value=broker_name)
-  ws.cell(row=row, column=2, value=suppliers_count)
-  row += 1
-  print("{} - {}".format(broker_name, suppliers_count))
+    ws.cell(row=row, column=1, value=broker_name)
+    ws.cell(row=row, column=2, value=suppliers_count)
+    row += 1
+    print("{} - {}".format(broker_name, suppliers_count))
 
 wb.save(result_file)

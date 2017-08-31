2b0cf7e4763827425ca9134a5d5409bfe8dc374c
Shevchenko Vlad <you@example.com>
2017-08-31 13:45:18 +0300

    Added selection of date, generation of hash, deletion of old reports

--- reports/brokers/api/views/reports_generator.py
+++ reports/brokers/api/views/reports_generator.py
@@ -1,8 +1,9 @@
 import os
 from shutil import copyfile
-from datetime import datetime
+from datetime import datetime, timedelta
 import mysql.connector as mariadb
 from openpyxl import load_workbook
+import hashlib
 from reports.brokers.api.selections import report1, report2, report3
 
 
@@ -15,11 +16,13 @@ class GeneratorOfReports:
         self.conn = mariadb.connect(host='127.0.0.1', user='root', password='root', database='reports_data',
                                     charset='utf8')
         self.cursor = self.conn.cursor(buffered=True)
-        self.cursor.execute(eval('report{}'.format(self.report_number)),
-                            {'start_date': self.start_report_period, 'end_date': self.end_report_period})
-
+        self.cursor.execute(eval('report{}'.format(self.report_number)), {'start_date': self.start_report_period,
+                                                                          'end_date': self.end_report_period})
         self.templates_dir = 'templates'
         self.result_dir = 'reports'
+
+        self.deleting_old_reports()
+
         self.template_file_name = '{}.xlsx'.format(self.report_number)
         self.t = os.path.splitext(self.template_file_name)
         self.result_file = os.path.join(self.result_dir, datetime.now().strftime('%Y-%m-%d-%H-%M-%S') +
@@ -29,7 +32,28 @@ class GeneratorOfReports:
         self.ws = self.wb.active
         eval('self.report_{}()'.format(self.report_number))
 
+    def deleting_old_reports(self):
+        for file in os.listdir(self.result_dir):
+            file_date = datetime.strptime(str(file.split('-report-number=')[0]), '%Y-%m-%d-%H-%M-%S')
+            now = datetime.now()
+            delta = now - file_date
+            if delta.days >= 1:
+                os.remove(os.path.abspath(os.path.join(self.result_dir, file)))
+
+    def get_hash_from_filename(self):
+        file_name = os.path.split(self.result_file)[1]
+        hasher = hashlib.md5(file_name)
+        return hasher.hexdigest()
+
+    def get_path_from_hash(self, hash_file):
+        for file in os.listdir(self.result_dir):
+            hasher = hashlib.md5(file)
+            if hasher.hexdigest() == hash_file:
+                return os.path.abspath(os.path.join(self.result_dir, file))
+        return 'Wrong hash or this file is deleted!'
+
     def report_1(self):
+
         for broker_name, suppliers_count in self.cursor:
             self.data.append([broker_name, suppliers_count])
 
@@ -41,11 +65,12 @@ class GeneratorOfReports:
             self.ws.cell(row=row, column=2, value=suppliers_count)
             row += 1
         self.wb.save(self.result_file)
+        return self.get_hash_from_filename()
 
-    def report_2(self, start_report_period, end_report_period):
+    def report_2(self):
         pass
 
-    def report_3(self, start_report_period, end_report_period):
+    def report_3(self):
         pass
 
 

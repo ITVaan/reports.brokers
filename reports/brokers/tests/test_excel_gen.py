ad4082c92a943d94af72a00d025c1a1ace9c2f71
dtrenkenshu <Cegthlfybk_1>
2017-09-04 10:46:42 +0300

    Db auth from file added to tests

--- reports/brokers/tests/test_excel_gen.py
+++ reports/brokers/tests/test_excel_gen.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+from ConfigParser import SafeConfigParser
 from datetime import datetime
 from shutil import copyfile
 from unittest import TestCase
@@ -6,7 +8,6 @@ import os
 import re
 
 import mysql.connector as mariadb
-from mysql.connector import OperationalError, ProgrammingError
 from openpyxl import load_workbook
 
 
@@ -30,44 +31,62 @@ def execute_scripts_from_file(cursor, filename):
                 continue
             cursor.execute(query)
 
+
 sql = """SELECT grp1.first_broker, COUNT(grp1.identifier) AS new_tenderers_count
 FROM
-	(
-		SELECT
-			ts.`identifier`,
-			(
-				SELECT br2.code
-				FROM
-					tenders t2
-					LEFT JOIN `brokers` br2 ON br2.`id` = t2.`broker_id`
-					LEFT JOIN bids b2 ON b2.`tender_id` = t2.id
-					LEFT JOIN `tenderers_bids` tb2 ON tb2.`bid_id` = b2.id
-				WHERE tb2.tenderer_id = ts.`id`
-				ORDER BY b2.bid_date
-				LIMIT 1) AS first_broker,
-				COUNT(t.id) AS tenders_count
-		FROM
-			tenders t
-			LEFT JOIN `brokers` br ON br.`id` = t.`broker_id`
-			LEFT JOIN bids b ON b.`tender_id` = t.id
-			LEFT JOIN `tenderers_bids` tb ON tb.`bid_id` = b.id
-			LEFT JOIN tenderers ts ON ts.`id` = tb.`tenderer_id`
-		WHERE ts.`id` IS NOT NULL
-		GROUP BY ts.`identifier`
-		HAVING COUNT(t.id) > 1
-		ORDER BY ts.`id`
-	) AS grp1
+    (
+        SELECT
+            ts.`identifier`,
+            (
+                SELECT br2.code
+                FROM
+                    tenders t2
+                    LEFT JOIN `brokers` br2 ON br2.`id` = t2.`broker_id`
+                    LEFT JOIN bids b2 ON b2.`tender_id` = t2.id
+                    LEFT JOIN `tenderers_bids` tb2 ON tb2.`bid_id` = b2.id
+                WHERE tb2.tenderer_id = ts.`id`
+                ORDER BY b2.bid_date
+                LIMIT 1) AS first_broker,
+                COUNT(t.id) AS tenders_count
+        FROM
+            tenders t
+            LEFT JOIN `brokers` br ON br.`id` = t.`broker_id`
+            LEFT JOIN bids b ON b.`tender_id` = t.id
+            LEFT JOIN `tenderers_bids` tb ON tb.`bid_id` = b.id
+            LEFT JOIN tenderers ts ON ts.`id` = tb.`tenderer_id`
+        WHERE ts.`id` IS NOT NULL
+        GROUP BY ts.`identifier`
+        HAVING COUNT(t.id) > 1
+        ORDER BY ts.`id`
+    ) AS grp1
 GROUP BY grp1.first_broker
 ORDER BY 2 DESC""";
 
 
-class TestDataBaseConnection(TestCase):
+def get_item_from_section(conf_parser, section, item):
+    """
+    :type conf_parser: SafeConfigParser
+    :param conf_parser: Config parser with file already read into it
+    :type section: str
+    :param section: String, name of section
+    :type item: str
+    :param item: String, name of option
+    :type return: str
+    :return: item or empty string if no such section
+    """
+    if conf_parser.has_option(section, item):
+        return conf_parser.get(section, item)
+    else:
+        return ''
 
+
+class TestDataBaseConnection(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.conn = mariadb.connect(host='127.0.0.1', user='root', password='111')
-        # cursor = cls.conn.cursor(buffered=True)
-        # cursor.close()
+        conf_parser = SafeConfigParser()
+        conf_parser.read("auth.ini")
+        password = get_item_from_section(conf_parser, "Database", "root_password")
+        cls.conn = mariadb.connect(host='127.0.0.1', user='root', password=password)
 
     @classmethod
     def tearDownClass(cls):
@@ -75,9 +94,7 @@ class TestDataBaseConnection(TestCase):
 
     def setUp(self):
         cursor = self.conn.cursor(buffered=True)
-
         cursor.execute("""DROP DATABASE IF EXISTS reports_data_test;""")
-        # import pdb;pdb.set_trace()
         cursor.execute("""CREATE DATABASE IF NOT EXISTS reports_data_test;""")
         cursor.execute("""USE reports_data_test""")
         execute_scripts_from_file(cursor, "reports/brokers/database/reports_data_dev.sql")
@@ -98,22 +115,15 @@ class TestDataBaseConnection(TestCase):
         templates_dir = 'reports/templates'
         result_dir = 'reports'
         template_file_name = 'one.xlsx'
-
         t = os.path.splitext(template_file_name)
         result_file = os.path.join(result_dir, t[0] + '-' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + t[1])
-
         copyfile(os.path.join(templates_dir, template_file_name), result_file)
-
         wb = load_workbook(filename=result_file)
         ws = wb.active
-
         row = 2
-        print(data)
-
         for (broker_name, suppliers_count) in data:
             ws.cell(row=row, column=1, value=broker_name)
             ws.cell(row=row, column=2, value=suppliers_count)
             row += 1
             print("{} - {}".format(broker_name, suppliers_count))
-
         wb.save(result_file)

6b31be22a37a73cc26bf1b2e62628d5f34e66830
Shevchenko Vlad <you@example.com>
2017-09-19 12:57:16 +0300

    Added tests for 3rd report

--- reports/brokers/tests/utils.py
+++ reports/brokers/tests/utils.py
@@ -3,10 +3,9 @@ from datetime import datetime
 from shutil import copyfile
 from uuid import uuid4
 
-import os
 from gevent import sleep as gsleep
 from openpyxl import Workbook, load_workbook
-from os import path
+import os
 
 from reports.brokers.tests.test_databridge.base import config
 
@@ -17,7 +16,7 @@ test_config = {
     'database': 'reports_data_test',
     'db_charset': 'utf8',
     'templates_dir': "reports/brokers/api/views/templates",
-    'result_dir': path.join(path.dirname(path.realpath(__file__)), "test_reports")
+    'result_dir': os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_reports")
 
 }
 
@@ -35,8 +34,8 @@ def filename(report_number, user_id, file_format):
 
 def copy_xls_file_from_template():
     template_file_name = '1.xlsx'
-    result_file = path.join(test_config.get('result_dir'), filename(1, 1, ".xlsx"))
-    copyfile(path.join(test_config.get('templates_dir'), template_file_name), result_file)
+    result_file = os.path.join(test_config.get('result_dir'), filename(1, 1, ".xlsx"))
+    copyfile(os.path.join(test_config.get('templates_dir'), template_file_name), result_file)
 
     return result_file
 
@@ -44,7 +43,7 @@ def copy_xls_file_from_template():
 def test_purge():
     for file in os.listdir(test_config.get('result_dir')):
         if ".xls" in file or ".xlsx" in file:
-            os.remove(path.join(test_config.get('result_dir'), file))
+            os.remove(os.path.join(test_config.get('result_dir'), file))
 
 
 def create_example_worksheet():
